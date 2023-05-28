87a3a7d1c60e8ad993b7b8cabdc374f542dbdd5a
Emmanuel Babatunde <iambabatundes@gmail.com>
2023-05-28 06:38:29 -0700

    deleting the deplication

--- public/categories.js
+++ public/categories.js
@@ -1,16 +1,3 @@
-let categories = [
-  { name: "Graphic Design", icon: "graphics-design.svg" },
-  { name: "Digital Marketing", icon: "online-marketing.svg" },
-  { name: "Writing & Translation", icon: "writing-translation.svg" },
-  { name: "Video & Animation", icon: "video-animation.svg" },
-  { name: "Music & Audio", icon: "music-audio.svg" },
-  { name: "Programming & Tech", icon: "programming.svg" },
-  { name: "Business", icon: "business.svg" },
-  { name: "Lifestyle", icon: "lifestyle.svg" },
-  { name: "Data", icon: "data.svg" },
-  { name: "Photography", icon: "photography.svg" },
-];
-export default categories;
 const categories = [
   { name: "Graphic Design", icon: "graphics-design.svg" },
   { name: "Digital Marketing", icon: "online-marketing.svg" },
@@ -23,4 +10,5 @@ const categories = [
   { name: "Data", icon: "data.svg" },
   { name: "Photography", icon: "photography.svg" },
 ];
+
 export default categories;
